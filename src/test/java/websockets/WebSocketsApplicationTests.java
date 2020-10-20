7ac21808b6a21a93a312615cc6afd6137dfa9618
prudhvir3ddy <prudhvireddy.m01@gmail.com>
2020-10-20 11:29:12 +0530

    first commit

---
+++ src/test/java/websockets/WebSocketsApplicationTests.java
@@ -0,0 +1,13 @@
+package websockets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
