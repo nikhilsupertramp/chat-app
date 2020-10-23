d0364c4353b1588b7cea8add62e1ff2faf8c55c8
Mona Lisa <sainikhil027@gmail.com>
2020-10-23 13:31:40 +0530

    First integration with frontend

--- src/main/java/websockets/config/WebSocketConfig.java
+++ src/main/java/websockets/config/WebSocketConfig.java
@@ -1,4 +1,27 @@
-package websockets.;
+package websockets.config;
 
-public class WebSocketConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/chat");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/socket")
+                .setAllowedOrigins("*")
+                .withSockJS();
+        //.withSockJS();
+    }
 }
--- src/main/java/websockets/controllers/WebSocketController.java
+++ src/main/java/websockets/controllers/WebSocketController.java
@@ -1,4 +1,28 @@
 package websockets.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Controller
 public class WebSocketController {
+    private final SimpMessagingTemplate template;
+
+    @Autowired
+    WebSocketController(SimpMessagingTemplate template){
+        this.template = template;
+    }
+
+    @MessageMapping("/send/message")
+    public void onReceivedMessage(String message){
+        this.template.convertAndSend("/chat",
+                new SimpleDateFormat("HH:mm:ss")
+                        .format(new Date()) + "- " + message);
+
+        System.out.println(message);
+    }
 }
